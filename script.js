b8a3afccbb7d66275fbdb9b7dccb174fc6d37499
Michael Bowman <Bowman@Michaels-iMac.local>
2021-03-20 20:31:44 -0700

    Moves all event listeners to document scope.

--- script.js
+++ script.js
@@ -1,207 +1,194 @@
-let nowPlaying = false;
-
-const menu = document.getElementById("menu");
-
-const newGameButton = document.getElementById("new-game");
-newGameButton.addEventListener("click", function () {
-  if (document.getElementById("strike") !== null) {
-    document.getElementById("strike").remove();
-  }
-  resetBoard();
-  printBoard();
-  getPlayers();
-});
-
-let players = [];
+let playerOne;
+let playerTwo;
 let currentPlayer;
+let nowPlaying = false;
+const squares = [];
 
-const playerFactory = (name, marker) => {
-  return {
-    name,
-    marker,
-  };
+const newSquare = (element) => {
+  const marker = '';
+  return { element, marker };
 };
 
-function getPlayers() {
-  menu.innerHTML = "";
-  let label = document.createElement("label");
-  label.innerHTML = "Player one name?";
+function resetBoard() {
+  while (squares.length > 0) {
+    squares.pop();
+  }
+  for (let i = 1; i < 10; i += 1) {
+    squares.push(newSquare(document.getElementById(`s${i}`)));
+    document.getElementById(`s${i}`).innerHTML = '';
+  }
+  if (document.getElementById('strike') !== null) {
+    document.getElementById('strike').remove();
+  }
+}
+
+const newPlayer = (name, marker) => ({
+  name, marker,
+});
+
+const capitalize = (s) => {
+  if (typeof s !== 'string') return '';
+  return s.charAt(0).toUpperCase() + s.slice(1);
+};
+
+function askForPlayer(number) {
+  let numberAsString = '';
+  if (number === 1) {
+    numberAsString = 'one';
+  } else if (number === 2) {
+    numberAsString = 'two';
+  }
+  const menu = document.getElementById('menu');
+  menu.innerHTML = '';
+  const label = document.createElement('label');
+  label.innerHTML = `Player ${numberAsString} name?`;
+  label.id = 'playerNameInputLabel';
   menu.appendChild(label);
-  let input = document.createElement("input");
-  input.type = "text";
+  const input = document.createElement('input');
+  input.type = 'text';
+  input.id = `player${capitalize(numberAsString)}NameInput`;
   menu.appendChild(input);
-  let button = document.createElement("button");
-  button.innerHTML = "Submit";
-  button.addEventListener("click", function () {
-    if (input.value !== "") {
-      if (players.length === 0) {
-        players.push(playerFactory(input.value, "X"));
-        label.innerHTML = "Player two name?";
-        input.value = "";
-      } else {
-        players.push(playerFactory(input.value, "O"));
-        currentPlayer = players[0];
-        nowPlaying = true;
-        whoseTurn();
-      }
-    } else {
-      label.innerHTML = "Please choose a valid player name.";
-    }
-  });
+  const button = document.createElement('button');
+  button.innerHTML = 'Submit';
+  button.id = `player${capitalize(numberAsString)}NameSubmit`;
   menu.appendChild(button);
 }
 
-function whoseTurn() {
-  menu.innerHTML = "";
-  let p = document.createElement("p");
-  p.innerHTML = currentPlayer["name"] + "'s turn";
+function printCurrentTurn() {
+  const menu = document.getElementById('menu');
+  menu.innerHTML = '';
+  const p = document.createElement('p');
+  p.innerHTML = `${currentPlayer.name}'s turn`;
   menu.appendChild(p);
 }
 
-const board = document.getElementById("board");
-const s1 = document.getElementById("s1");
-const s2 = document.getElementById("s2");
-const s3 = document.getElementById("s3");
-const s4 = document.getElementById("s4");
-const s5 = document.getElementById("s5");
-const s6 = document.getElementById("s6");
-const s7 = document.getElementById("s7");
-const s8 = document.getElementById("s8");
-const s9 = document.getElementById("s9");
-
-let gameBoard;
-
-function resetBoard() {
-  gameBoard = [
-    { element: s1, marker: "" },
-    { element: s2, marker: "" },
-    { element: s3, marker: "" },
-    { element: s4, marker: "" },
-    { element: s5, marker: "" },
-    { element: s6, marker: "" },
-    { element: s7, marker: "" },
-    { element: s8, marker: "" },
-    { element: s9, marker: "" },
-  ];
+function drawStrike(className) {
+  const strike = document.createElement('div');
+  strike.id = 'strike';
+  strike.className = className;
+  document.getElementById('board').appendChild(strike);
 }
 
-resetBoard();
-
-for (let i = 0; i < gameBoard.length; i++) {
-  gameBoard[i]["element"].addEventListener("click", function () {
-    if (nowPlaying === false) {
-      return;
-    }
-    if (gameBoard[i]["marker"] === "") {
-      gameBoard[i]["marker"] = currentPlayer.marker;
-      printBoard();
-      evaluateBoard();
-      if (nowPlaying === false) {
-        return;
-      }
-      if (currentPlayer === players[0]) {
-        currentPlayer = players[1];
-      } else {
-        currentPlayer = players[0];
-      }
-      whoseTurn();
-    }
-  });
-}
-
-function printBoard() {
-  for (let i = 0; i < gameBoard.length; i++) {
-    gameBoard[i]["element"].innerHTML = "";
-    if (gameBoard[i]["marker"] === "X") {
-      let div = document.createElement("div");
-      div.className = "marker-x";
-      gameBoard[i]["element"].appendChild(div);
-    } else if (gameBoard[i]["marker"] === "O") {
-      let div = document.createElement("div");
-      div.className = "marker-o";
-      gameBoard[i]["element"].appendChild(div);
-    }
+function announceWinner(playerMarker) {
+  let player;
+  if (playerMarker === 'x') {
+    player = playerOne;
+  } else if (playerMarker === 'o') {
+    player = playerTwo;
   }
-}
-
-function announceWinner(marker) {
-  menu.innerHTML = "";
-  let p = document.createElement("p");
-  if (marker === "X") {
-    p.innerHTML = players[0]["name"] + " wins!";
-  } else if (marker === "O") {
-    p.innerHTML = players[1]["name"] + " wins!";
+  const menu = document.getElementById('menu');
+  menu.innerHTML = '';
+  const p = document.createElement('p');
+  if (playerMarker === 'draw') {
+    p.innerHTML = 'Draw.';
   } else {
-    p.innerHTML = "Draw";
+    p.innerHTML = `${player.name} wins!`;
   }
   menu.appendChild(p);
-  nowPlaying = false;
-  players = [];
+  const newGameButton = document.createElement('button');
+  newGameButton.id = 'new-game';
+  newGameButton.innerHTML = 'New Game';
   menu.appendChild(newGameButton);
+  playerOne = '';
+  playerTwo = '';
+  currentPlayer = '';
+  nowPlaying = false;
 }
 
 function evaluateBoard() {
   for (let i = 0; i < 9; i += 3) {
-    if (gameBoard[i]["marker"] !== "") {
+    if (squares[i].marker !== '') {
       if (
-        gameBoard[i]["marker"] === gameBoard[i + 1]["marker"] &&
-        gameBoard[i]["marker"] === gameBoard[i + 2]["marker"]
+        squares[i].marker === squares[i + 1].marker
+          && squares[i].marker === squares[i + 2].marker
       ) {
-        announceWinner(gameBoard[i]["marker"]);
-        let strike = document.createElement("div");
-        strike.id = "strike";
-        strike.className = "h" + i;
-        board.appendChild(strike);
+        announceWinner(squares[i].marker);
+        drawStrike(`h${i}`);
         return;
       }
     }
   }
-  for (let i = 0; i < 3; i++) {
-    if (gameBoard[i]["marker"] !== "") {
+  for (let i = 0; i < 3; i += 1) {
+    if (squares[i].marker !== '') {
       if (
-        gameBoard[i]["marker"] === gameBoard[i + 3]["marker"] &&
-        gameBoard[i]["marker"] === gameBoard[i + 6]["marker"]
+        squares[i].marker === squares[i + 3].marker
+          && squares[i].marker === squares[i + 6].marker
       ) {
-        announceWinner(gameBoard[i]["marker"]);
-        let strike = document.createElement("div");
-        strike.id = "strike";
-        strike.className = "v" + i;
-        board.appendChild(strike);
+        announceWinner(squares[i].marker);
+        drawStrike(`v${i}`);
         return;
       }
     }
   }
-  if (gameBoard[4]["marker"] !== "") {
+  if (squares[4].marker !== '') {
     if (
-      gameBoard[4]["marker"] === gameBoard[0]["marker"] &&
-      gameBoard[4]["marker"] === gameBoard[8]["marker"]
+      squares[4].marker === squares[0].marker
+        && squares[4].marker === squares[8].marker
     ) {
-      announceWinner(gameBoard[4]["marker"]);
-      let strike = document.createElement("div");
-      strike.id = "strike";
-      strike.className = "dccw";
-      board.appendChild(strike);
+      announceWinner(squares[4].marker);
+      drawStrike('dccw');
       return;
-    } else if (
-      gameBoard[4]["marker"] === gameBoard[2]["marker"] &&
-      gameBoard[4]["marker"] === gameBoard[6]["marker"]
+    } if (
+      squares[4].marker === squares[2].marker
+        && squares[4].marker === squares[6].marker
     ) {
-      announceWinner(gameBoard[4]["marker"]);
-      let strike = document.createElement("div");
-      strike.id = "strike";
-      strike.className = "dcw";
-      board.appendChild(strike);
+      announceWinner(squares[4].marker);
+      drawStrike('dcw');
       return;
     }
   }
   let filledSquares = 0;
-  for (let i = 0; i < 9; i++) {
-    if (gameBoard[i]["marker"] !== "") {
+  for (let i = 0; i < 9; i += 1) {
+    if (squares[i].marker !== '') {
       filledSquares += 1;
     }
   }
   if (filledSquares === 9) {
-    announceWinner();
-    return;
+    announceWinner('draw');
+  } else if (currentPlayer === playerOne) {
+    currentPlayer = playerTwo;
+    printCurrentTurn();
+  } else if (currentPlayer === playerTwo) {
+    currentPlayer = playerOne;
+    printCurrentTurn();
   }
 }
+
+document.addEventListener('click', (e) => {
+  const newGameButton = document.getElementById('new-game');
+  const playerNameInputLabel = document.getElementById('playerNameInputLabel');
+  const playerOneNameInput = document.getElementById('playerOneNameInput');
+  const playerOneNameSubmit = document.getElementById('playerOneNameSubmit');
+  const playerTwoNameInput = document.getElementById('playerTwoNameInput');
+  const playerTwoNameSubmit = document.getElementById('playerTwoNameSubmit');
+  if (e.path.includes(newGameButton)) {
+    resetBoard();
+    askForPlayer(1);
+  } else if (e.path.includes(playerOneNameSubmit)) {
+    if (playerOneNameInput.value.trim() === '') {
+      playerNameInputLabel.innerHTML = 'Please choose a valid player name.';
+    } else {
+      playerOne = newPlayer(playerOneNameInput.value, 'x');
+      askForPlayer(2);
+    }
+  } else if (e.path.includes(playerTwoNameSubmit)) {
+    if (playerTwoNameInput.value.trim() === '') {
+      playerNameInputLabel.innerHTML = 'Please choose a valid player name.';
+    } else {
+      playerTwo = newPlayer(playerTwoNameInput.value, 'o');
+      nowPlaying = true;
+      currentPlayer = playerOne;
+      printCurrentTurn();
+    }
+  } else if (nowPlaying === true) {
+    for (let i = 1; i < 10; i += 1) {
+      if (e.path.includes(document.getElementById(`s${i}`)) && (squares[i - 1].marker === '')) {
+        squares[i - 1].marker = currentPlayer.marker;
+        const markerDiv = document.createElement('div');
+        markerDiv.className = `marker-${currentPlayer.marker}`;
+        document.getElementById(`s${i}`).appendChild(markerDiv);
+        evaluateBoard();
+      }
+    }
+  }
+});
